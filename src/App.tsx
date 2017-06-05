4aa9435c9da221b763d1617d529d1a4a9af032a3
MadloptD <madlopt@gmail.com>
2017-06-05 15:29:38 +0300

    Add slider switch and horizontal layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import {AppRegistry} from "react-native";
 import {HomePage} from "./pages/home/HomePage";
-import React, {Component} from "react";
+//import React, {Component} from "react";
 
-export class App extends Component<{}, {}> {
-    render(): JSX.Element {
-        return (
-           <HomePage/>
-        );
-    }
-}
-AppRegistry.registerComponent("Layout", () => App);
+// export class App extends Component<{}, {}> {
+//     render(): JSX.Element {
+//         return (
+//            <HomePage/>
+//         );
+//     }
+// }
+AppRegistry.registerComponent("Layout", () => HomePage);
--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -1,46 +1,131 @@
 import React, {Component} from "react";
-import {Text, View, StyleSheet, ViewStyle} from "react-native";
-import {FlexAlign} from "../../types/react-styles";
+//import {Text, View, StyleSheet, ViewStyle} from "react-native";
+//import {FlexAlign} from "../../types/react-styles";
+import {View, Text, TextInput, Slider, Switch} from "react-native";
 
 interface IProps {
+    text: string;
 }
 
 interface IState {
+    showText: boolean;
 }
 
-export class HomePage extends Component<IProps, IState> {
+// class Blink extends Component<IProps, IState> {
+//     constructor(props: IProps) {
+//         super(props);
+//         this.state = {showText: true};
+//         setInterval(() => {
+//             this.setState({showText: !this.state.showText});
+//         }, 1000);
+//     }
+//
+//     render(): JSX.Element {
+//         let display = this.state.showText ? this.props.text : ' ';
+//
+//         return (
+//             <Text>{display}</Text>
+//         );
+//     }
+// }
+
+interface IPropsTranslate {
+
+}
+
+interface IStateTranslate {
+    value?: string;
+}
+
+export class Translator extends Component<IPropsTranslate, IStateTranslate> {
+    constructor(props: IPropsTranslate) {
+        super(props);
+        this.state = {
+            value: "blabla"
+        };
+    }
+
+    onChange = (text: string): void => {
+        this.setState({value: text});
+    }
 
     render(): JSX.Element {
         return (
-            <View style={styles.container}>
-                <Text style={styles.welcome}>
-                    Welcome to React Native!
-                </Text>
-                <Text style={styles.instructions}>
-                    To get started, edit src/pages/App.tsx
-                </Text>
-                <Text style={styles.instructions}>
-                    Double tap R on your keyboard to reload,{'\n'}
-                    Shake or press menu button for dev menu
-                </Text>
+        <View>
+            <TextInput
+                style={{height: 40}}
+                placeholder="input some text"
+                onChangeText={this.onChange}
+            />
+            <Text style={{padding: 10, fontSize: 48}}>
+                {this.state.value}
+            </Text>
+        </View>
+        );
+    }
+}
+
+interface IPropsSlider {
+
+}
+
+interface IStateSlider {
+    value?: number;
+    SwitchIsOn: boolean;
+}
+
+class SliderExample extends React.Component<IPropsSlider, IStateSlider> {
+    constructor(props: IPropsTranslate) {
+        super(props);
+        this.state = {
+            value: 1,
+            SwitchIsOn: true
+        };
+    }
+
+    onChange = (value: number): void => this.setState({value: value});
+    showthis(): string {
+        return "";
+    }
+
+    render(): JSX.Element {
+        return (
+            <View>
+                {/*<Text style={{height: 40}} >*/}
+                    {/*{this.state.value && +this.state.value.toFixed(3)}*/}
+                {/*</Text>*/}
+                <Slider
+                    {...this.props}
+                    minimumValue = {1}
+                    maximumValue = {7}
+                    onValueChange={this.onChange} />
+                <Switch
+                    onValueChange={(value) => this.setState({SwitchIsOn: value})}
+                    style={{marginBottom: 10}}
+                    value={this.state.SwitchIsOn} />
+                <View style={{flexDirection: 'row'}}>
+                    <View style={{flex: this.state.value && +this.state.value.toFixed(3), height: 100, backgroundColor: 'red'}} />
+                    <View style={{flex: 1, height: 100, backgroundColor: 'green'}} />
+                    <View style={{flex: 1, height: 100, backgroundColor: 'blue'}} />
+                </View>
 
             </View>
         );
     }
 }
 
-const styles = StyleSheet.create({
-    container: {
-        alignItems: FlexAlign.center
-    } as ViewStyle,
-    welcome: {
-        fontSize: 20,
-        textAlign: FlexAlign.center,
-        margin: 10,
-    } as ViewStyle,
-    instructions: {
-        textAlign: 'center',
-        color: '#333333',
-        marginBottom: 5,
-    } as ViewStyle
-});
+
+
+
+export class HomePage extends Component<IProps, IState> {
+
+    render(): JSX.Element {
+        return (
+            <View>
+                <SliderExample/>
+            </View>
+        );
+    }
+}
+
+
