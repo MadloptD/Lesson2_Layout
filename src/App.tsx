e8f3d3a9bd36e5d954e4da45b80367add79c54dd
MadloptD <madlopt@gmail.com>
2017-06-05 10:13:57 +0300

    add flex squares and try blink text

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
 import {AppRegistry} from "react-native";
 import {HomePage} from "./pages/home/HomePage";
+import React, {Component} from "react";
 
-
-AppRegistry.registerComponent("ReactNativeBeginnerTutorialApp", () => HomePage);
+export class App extends Component<{}, {}> {
+    render(): JSX.Element {
+        return (
+           <HomePage/>
+        );
+    }
+}
+AppRegistry.registerComponent("Layout", () => App);
